d46e2a4ecd7e1f3c2751244926ee9b61f891d261
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-11-29 23:32:53 +0900

    Delete variables.tf

--- 03.terraform-state/file-layout-example/global/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket. Must be globally unique."
-  type        = string
-}
-
-variable "table_name" {
-  description = "The name of the DynamoDB table. Must be unique in this AWS account."
-  type        = string
-}
