bfc510c7b92be5fc4bb31b2654ad0d4a5956180b
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-11-29 22:46:42 +0900

    Create main.tf

---
+++ 03.terraform-state/3.2.state-file/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
+  # 상태 파일의 이력관리를 위해
+  # 버전관리 기능을 활성화 합니다.
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
