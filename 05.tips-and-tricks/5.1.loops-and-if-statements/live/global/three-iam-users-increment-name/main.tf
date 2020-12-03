ef140b452042a2bb25e5a8e5f924a1bcdc4498f2
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-12-03 23:37:09 +0900

    Create main.tf

---
+++ 05.tips-and-tricks/5.1.loops-and-if-statements/live/global/three-iam-users-increment-name/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  # 2.x 버전의 AWS 공급자 허용
+  version = "~> 2.0"
+}
+
+resource "aws_iam_user" "example" {
+  count = 3
+
+  name  = "${var.user_name_prefix}.${count.index}"
+}
+
