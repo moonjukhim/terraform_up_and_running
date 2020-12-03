e322e95c45734baee17bfd7ef6e0e721c97861b4
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-12-03 23:20:16 +0900

    Update main.tf

--- 05.tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
+++ 05.tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_version = ">= 0.12, < 0.13"
 }
@@ -5,13 +7,13 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 
-  # Allow any 2.x version of the AWS provider
+  # 2.x 버전의 AWS 공급자 허용
   version = "~> 2.0"
 }
 
 resource "aws_iam_user" "existing_user" {
-  # You should change this to the username of an IAM user that already
-  # exists so you can practice using the terraform import command
-  name = "yevgeniy.brikman"
+  # terraform import 명령 사용을 연습 할 수 있도록 
+  # 이미 존재하는 IAM 사용자의 사용자 이름으로 변경해야합니다.
+  name = "terraform.user"
 }
 
