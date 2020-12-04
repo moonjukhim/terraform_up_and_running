c5d7e9e75160e1878740f55b58c7e1f74fd636fe
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-12-04 23:00:59 +0900

    Update variables.tf

--- 05.tips-and-tricks/5.3.zero-downtime-deployment/live/stage/services/webserver-cluster/variables.tf
+++ 05.tips-and-tricks/5.3.zero-downtime-deployment/live/stage/services/webserver-cluster/variables.tf
@@ -21,7 +21,7 @@ variable "db_remote_state_key" {
 variable "cluster_name" {
   description = "The name to use to namespace all the resources in the cluster"
   type        = string
-  default     = "webservers-stage"
+  default     = "webservers-prod"
 }
 
 variable "server_text" {
