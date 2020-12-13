f6820e6594dd85b4e9d660f61f61dfbc70e74986
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2020-12-13 22:59:36 +0900

    Create setup-webserver.sh

---
+++ 01.why-terraform/bash/setup-webserver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# A script that configures a web server
+
+set -e
+
+# Update the apt-get cache
+sudo apt-get update
+
+# Install PHP and Apache
+sudo apt-get install -y php apache2
+
+# Copy the code from the repository
+sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
+
+# Start Apache
+sudo service apache2 start
